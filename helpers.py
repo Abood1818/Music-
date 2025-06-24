cfd3ab3b903d536f2357db3008876966c2bbb94c
Abood1818 <abdualmalik350@gmail.com>
2025-06-24 22:16:21 +0300

    Update helpers.py

--- helpers.py
+++ helpers.py
@@ -1,1 +1,15 @@
+import yt_dlp
 
+def download_audio(url):
+    ydl_opts = {
+        'format': 'bestaudio',
+        'outtmpl': 'downloads/%(title)s.%(ext)s',
+        'noplaylist': True,
+        'quiet': True,
+        'no_warnings': True,
+        'default_search': 'ytsearch',
+    }
+
+    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+        info = ydl.extract_info(url, download=True)
+        return ydl.prepare_filename(info)
